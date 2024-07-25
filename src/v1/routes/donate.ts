41e90ed0ff82f267399743efa7a76315a203306c
Tharindu Nimesh <127307898+TharinduNimesh@users.noreply.github.com>
2024-07-26 05:20:15 +0530

    complete donation

--- src/v1/routes/donate.ts
+++ src/v1/routes/donate.ts
@@ -6,13 +6,31 @@ import { createOrder, generateAccessToken } from "../functions/paypal";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
+app.get("/", async (c) => {
+  const adapter = new PrismaD1(c.env.DB);
+  const prisma = new PrismaClient({ adapter });
+
+  const donations = await prisma.donations.findMany({
+    orderBy: {
+      amount: "desc",
+    },
+  });
+  return c.json({
+    donations,
+  });
+});
+
 app.post("/", async (c) => {
-  const { amount } = await c.req.json<{ amount: number }>();
+  const request = await c.req.json<{
+    name: string;
+    email: string;
+    amount: number;
+  }>();
   const accessToken = await generateAccessToken(
     c.env.PAYPAL_CLIENT_ID,
     c.env.PAYPAL_CLIENT_SECRET
   );
-  const order = await createOrder(accessToken, amount);
+  const order = await createOrder(accessToken, request);
 
   return c.json({
     response: order,
@@ -22,6 +40,8 @@ app.post("/", async (c) => {
 app.get("/payment-success", async (c) => {
   const adapter = new PrismaD1(c.env.DB);
   const prisma = new PrismaClient({ adapter });
+  const name = c.req.query("name");
+  const email = c.req.query("email");
   const token = c.req.query("token");
 
   const accessToken = await generateAccessToken(
@@ -42,21 +62,41 @@ app.get("/payment-success", async (c) => {
     );
 
     const data = await response.json<PayPalResponse>();
-    const name = data.purchase_units[0].shipping.name.full_name;
-    const email = data.payment_source.paypal.email_address;
+    // const name = data.purchase_units[0].shipping.name.full_name;
+    // const email = data.payment_source.paypal.email_address;
     const amount = data.purchase_units[0].payments.captures[0].amount.value;
 
-    await prisma.donations.create({
+    console.log(data);
+
+    const payment = await prisma.donations.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (!payment) {
+      await prisma.donations.create({
+        data: {
+          name: name as string,
+          email: email as string,
+          amount: parseFloat(amount),
+        },
+      });
+
+      return c.redirect(c.env.FRONTEND_URL + "/donation/success");
+    }
+
+    await prisma.donations.update({
+      where: {
+        id: payment.id,
+      },
       data: {
-        name,
-        email,
-        amount: parseFloat(amount),
+        amount: payment.amount + parseFloat(amount),
       },
     });
 
     return c.redirect(c.env.FRONTEND_URL + "/donation/success");
   } catch (error) {
-    return c.redirect(c.env.FRONTEND_URL + "/donation");
+    return c.redirect(c.env.FRONTEND_URL + "/donation/cancel");
   }
 });
 
